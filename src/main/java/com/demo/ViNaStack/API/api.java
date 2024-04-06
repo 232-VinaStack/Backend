414cfdeb49969d9d245f9ca192dfd217de43dc84
Don <daoduylongpy@gamil.com>
2024-04-06 13:29:21 +0700

    api

--- src/main/java/com/demo/ViNaStack/API/api.java
+++ src/main/java/com/demo/ViNaStack/API/api.java
@@ -2,19 +2,17 @@ package com.demo.ViNaStack.API;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RestController
-@CrossOrigin(origins = "http://10.128.180.40:5173")
+
 @Configuration
 public class api implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://*")
+                .allowedOrigins("http://localhost:3001/")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
