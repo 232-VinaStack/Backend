da26a12ee73b7b11c0aa61a1780f78ba83f2e1c4
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-04-06 10:48:07 +0700

    Add symptoms

--- src/main/java/com/demo/ViNaStack/Model/Appoinment.java
+++ src/main/java/com/demo/ViNaStack/Model/Appoinment.java
@@ -11,59 +11,62 @@ public class Appoinment {
     private int id;
     private int doctor_id;
     private String appointment_time;
-    @ElementCollection(targetClass = String.class)
-    private List<String> symptoms;
+    @ManyToMany(cascade = CascadeType.PERSIST) // For persisting symptoms automatically
+    @JoinTable(name = "appointment_symptoms", // Define join table name
+            joinColumns = @JoinColumn(name = "appointment_id"),
+            inverseJoinColumns = @JoinColumn(name = "symptom_id"))
+    private List<Symptom> symptoms;
     private int patient_id;
-
-    public Appoinment() {
-
+    
+    public Appoinment(int id, int doctor_id, String appointment_time, List<Symptom> symptoms, int patient_id) {
+        this.id = id;
+        this.doctor_id = doctor_id;
+        this.appointment_time = appointment_time;
+        this.symptoms = symptoms;
+        this.patient_id = patient_id;
     }
-
+    
+    public Appoinment() {
+    
+    }
+    
     public int getId() {
         return id;
     }
-
+    
     public void setId(int id) {
         this.id = id;
     }
-
+    
     public int getDoctor_id() {
         return doctor_id;
     }
-
+    
     public void setDoctor_id(int doctor_id) {
         this.doctor_id = doctor_id;
     }
-
+    
     public String getAppointment_time() {
         return appointment_time;
     }
-
-    public void setAppointment_time(String appoinment_time) {
-        this.appointment_time = appoinment_time;
+    
+    public void setAppointment_time(String appointment_time) {
+        this.appointment_time = appointment_time;
     }
-
-    public List<String> getSymptoms() {
+    
+    public List<Symptom> getSymptoms() {
         return symptoms;
     }
-
-    public void setSymptoms(List<String> symptoms) {
+    
+    public void setSymptoms(List<Symptom> symptoms) {
         this.symptoms = symptoms;
     }
-
+    
     public int getPatient_id() {
         return patient_id;
     }
-
+    
     public void setPatient_id(int patient_id) {
-        this.patient_id = patient_id;
-    }
-
-    public Appoinment(int id, int doctor_id, String appoinment_time, List<String> symptoms, int patient_id) {
-        this.id = id;
-        this.doctor_id = doctor_id;
-        this.appointment_time = appoinment_time;
-        this.symptoms = symptoms;
         this.patient_id = patient_id;
     }
 }
