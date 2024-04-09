fcaf129740e202a8459bf52ab07545512c82d3e2
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-04-09 15:16:35 +0700

    Add appointment_date

--- src/main/java/com/demo/ViNaStack/Model/Appointment.java
+++ src/main/java/com/demo/ViNaStack/Model/Appointment.java
@@ -11,18 +11,12 @@ public class Appointment {
     private int id;
     private int doctor_id;
     private String start_time;
-    private String end_time;
-    @ManyToMany
-    @JoinTable(name = "appointment_symptoms",
-            joinColumns = @JoinColumn(name = "appointment_id"),
-            inverseJoinColumns = @JoinColumn(name = "symptom_id"))
-    private List<Symptom> symptoms;
-    private int patient_id;
     
-    public Appointment(int doctor_id, String start_time, String end_time, List<Symptom> symptoms, int patient_id) {
+    public Appointment(int doctor_id, String start_time, String end_time, String appointment_date, List<Symptom> symptoms, int patient_id) {
         this.doctor_id = doctor_id;
         this.start_time = start_time;
         this.end_time = end_time;
+        this.appointment_date = appointment_date;
         this.symptoms = symptoms;
         this.patient_id = patient_id;
     }
@@ -63,6 +57,14 @@ public class Appointment {
         this.end_time = end_time;
     }
     
+    public String getAppointment_date() {
+        return appointment_date;
+    }
+    
+    public void setAppointment_date(String appointment_date) {
+        this.appointment_date = appointment_date;
+    }
+    
     public List<Symptom> getSymptoms() {
         return symptoms;
     }
@@ -78,4 +80,15 @@ public class Appointment {
     public void setPatient_id(int patient_id) {
         this.patient_id = patient_id;
     }
+    
+    private String end_time;
+    private String appointment_date;
+    @ManyToMany
+    @JoinTable(name = "appointment_symptoms",
+            joinColumns = @JoinColumn(name = "appointment_id"),
+            inverseJoinColumns = @JoinColumn(name = "symptom_id"))
+    private List<Symptom> symptoms;
+    private int patient_id;
+    
+  
 }
